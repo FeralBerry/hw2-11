615943461fc5fb40f25644f7e5fb44465598a938
FeralHorse <pusiket90@yandex.ru>
2023-12-26 14:51:22 +0300

    Переделал на использование аннотации Спринга без явного объявления сессии

--- src/main/java/org/example/Services/OrderService.java
+++ src/main/java/org/example/Services/OrderService.java
@@ -1,28 +1,20 @@
 package org.example.Services;
 
-import com.google.gson.Gson;
+import org.example.Cart;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class OrderService {
-    private final Map<String, List<Integer>> cart = new HashMap<>();
-    public List<Integer> add(int id, String sessionName){
-        List<Integer> listA = new ArrayList<>();
-        if(cart.get(sessionName) == null){
-            listA.add(id);
-        } else {
-            listA = cart.get(sessionName);
-            listA.add(id);
-        }
-        cart.put(sessionName,listA);
-        return cart.get(sessionName);
+    private final Cart cart;
+    public OrderService(Cart cart){
+        this.cart = cart;
     }
-    public String get(String sessionName){
-        return new Gson().toJson(cart.get(sessionName));
+    public List<Integer> add(List<Integer> ids){
+        return cart.addItems(ids);
+    }
+    public List<Integer> get(){
+        return cart.getItems();
     }
 }
--- src/main/java/org/example/Store/OrderController.java
+++ src/main/java/org/example/Store/OrderController.java
@@ -1,13 +1,9 @@
 package org.example.Store;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.example.Services.OrderService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/store/order/")
@@ -17,24 +13,11 @@ public class OrderController {
         this.orderService = orderService;
     }
     @GetMapping("/add")
-    public List<Integer> add(HttpServletRequest request, @RequestParam (value = "id") int id){
-        HttpSession session = request.getSession();
-        // Store data in the session
-        if(session.getAttribute("username") == null){
-            session.setAttribute("username", RandomGenerate());
-        }
-        return orderService.add(id, (String) session.getAttribute("username"));
+    public List<Integer> add(@RequestParam List<Integer> ids){
+        return orderService.add(ids);
     }
     @GetMapping("/get")
-    public String get(HttpServletRequest request){
-        HttpSession session = request.getSession();
-        return orderService.get((String) session.getAttribute("username"));
-    }
-    private String RandomGenerate(){
-        String symbols = "abcdefghijklmnopqrstuvwxyz";
-        return new Random().ints(50, 0, symbols.length())
-                .mapToObj(symbols::charAt)
-                .map(Object::toString)
-                .collect(Collectors.joining());
+    public List<Integer> get(){
+        return orderService.get();
     }
 }
